8cccfcf1e528ee4785ba6b4cdfa455c504c67a86
Marion Poljakov <marion.poljakov@gmail.com>
2024-09-06 10:28:07 +0300

    EX 26

--- src/class1/name.java
+++ src/class1/name.java
@@ -1,14 +1,21 @@
 package class1;
 
+import java.util.Scanner;
+
 public class name {
     public static void main(String[] args) {
-        int days = 365;
-        int seconds = 60;
-        int minutes = 60;
-        int hours = 24;
-        int sum = (seconds * minutes * hours) * days;
+        Scanner reader = new Scanner(System.in);
+        int sum = 0;
+        while (true) {
+            int read = Integer.parseInt(reader.nextLine());
+            if (read == 0) {
+                break;
+            }
 
+            sum = sum + read;
 
-        System.out.printf("There are " + sum + " seconds in a year" );
+            System.out.printf("Sum now: " + sum);
+        }
+        System.out.printf("Sum in the end: " + sum );
     }
 }
