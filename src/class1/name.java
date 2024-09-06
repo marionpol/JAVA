7a733ef381178924cba9585e9ac3ad050320310f
Marion Poljakov <marion.poljakov@gmail.com>
2024-09-06 10:10:10 +0300

    EX 5

--- src/class1/name.java
+++ src/class1/name.java
@@ -2,6 +2,13 @@ package class1;
 
 public class name {
     public static void main(String[] args) {
-        System.out.printf("Marion Poljakov");
+        int days = 365;
+        int seconds = 60;
+        int minutes = 60;
+        int hours = 24;
+        int sum = (seconds * minutes * hours) * days;
+
+
+        System.out.printf("There are " + sum + " seconds in a year" );
     }
 }
