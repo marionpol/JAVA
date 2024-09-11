cf3123a45fd3da883852131977efdb491728a1e0
Marion Poljakov <marion.poljakov@gmail.com>
2024-09-11 14:42:51 +0300

    Ex 97

---
+++ src/class1/ex97.java
@@ -0,0 +1,16 @@
+package class1;
+
+public class ex97 {
+    public static void main(String[] args) {
+        int[] array = {5, 1, 3, 4, 2};
+        printElegantly(array);
+    }
+    public static void printElegantly(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            if (i < array.length - 1) {
+                System.out.print(", ");
+            }
+        }
+    }
+}
